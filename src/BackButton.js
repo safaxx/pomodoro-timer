100b03dfd2d3aa4602aaff9998859a035ee6ef0a
Safa Khan <asafa2610@gmail.com>
2026-05-22 12:52:27 +0530

    fixed settings.js css

--- src/BackButton.js
+++ src/BackButton.js
@@ -1,6 +1,6 @@
-function BackButton({ onClick }) {
+function BackButton({ onClick, className = "" }) {
   return (
-    <button onClick={onClick} className="with-text">
+    <button onClick={onClick} className={`with-text ${className}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
--- src/Settings.js
+++ src/Settings.js
@@ -3,26 +3,29 @@ import BackButton from "./BackButton";
 function Settings({ focusMinutes, breakMinutes, onFocusChange, onBreakChange, onBack }) {
   return (
     <div className="settings">
-      <BackButton onClick={onBack} />
-      <h2>Time (minutes)</h2>
-      <div className="settings-grid">
-        <div className="settings-item">
-          <label>Focus</label>
-          <input
-            type="number"
-            min="1"
-            value={focusMinutes}
-            onChange={(e) => onFocusChange(Number(e.target.value))}
-          />
-        </div>
-        <div className="settings-item">
-          <label>Break</label>
-          <input
-            type="number"
-            min="1"
-            value={breakMinutes}
-            onChange={(e) => onBreakChange(Number(e.target.value))}
-          />
+      <BackButton onClick={onBack} className="settings-back" />
+      <h2 style={{color: "white"}}>Time (minutes)</h2>
+
+      <div className="settings-box">
+        <div className="settings-grid">
+          <div className="settings-item">
+            <label>Focus</label>
+            <input
+              type="number"
+              min="1"
+              value={focusMinutes}
+              onChange={(e) => onFocusChange(Number(e.target.value))}
+            />
+          </div>
+          <div className="settings-item">
+            <label>Break</label>
+            <input
+              type="number"
+              min="1"
+              value={breakMinutes}
+              onChange={(e) => onBreakChange(Number(e.target.value))}
+            />
+          </div>
         </div>
       </div>
     </div>
